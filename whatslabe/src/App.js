f473a065c3ee68a6a3722320de72f7cf97685aec
Ana Julia Lopes <anajulialopes@Anas-MacBook-Air.local>
2020-10-22 15:24:15 -0300

    Criar o JSX para estruturação da página

---
+++ whatslabe/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="campoDeMensagem">
+      <main className="main">
+        <section className="menu"></section>
+        <section className="conteudo"><input type="text" placeholder="Usuário" className="eAHCAl" /><input type="text" placeholder="Mensagem" className="kExHMy"/><button className="hiyNhT">Enviar</button></section>
+        <section className="links"></section>
+      </main>
+    </div>
+    
+  );
+}
+
+export default App;
